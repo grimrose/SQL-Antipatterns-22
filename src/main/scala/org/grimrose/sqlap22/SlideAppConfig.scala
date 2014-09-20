53f3848ff291e65fb319ca12186f687f8e0dd50a
grimrose <grwin7@gmail.com>
2014-09-20 21:58:10 +0900

    Update h2 console use in development

--- src/main/scala/org/grimrose/sqlap22/SlideAppConfig.scala
+++ src/main/scala/org/grimrose/sqlap22/SlideAppConfig.scala
@@ -3,15 +3,17 @@ package org.grimrose.sqlap22
 import org.h2.server.web.WebServlet
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.context.embedded.ServletRegistrationBean
-import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
+import org.springframework.context.annotation.{Bean, ComponentScan, Configuration, Profile}
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 class SlideAppConfig {
 
+  @Profile(Array("dev"))
   @Bean
   def h2servletRegistration: ServletRegistrationBean = {
     new ServletRegistrationBean(new WebServlet, "/console/*")
   }
+
 }
