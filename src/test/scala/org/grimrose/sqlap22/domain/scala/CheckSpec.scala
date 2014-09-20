44c26c9f5d86867235aad6567360defb866a51d4
grimrose <grwin7@gmail.com>
2014-09-20 23:19:30 +0900

    Update FunSpec to FlatSpec

--- src/test/scala/org/grimrose/sqlap22/domain/scala/CheckSpec.scala
+++ src/test/scala/org/grimrose/sqlap22/domain/scala/CheckSpec.scala
@@ -8,7 +8,7 @@ import scalikejdbc._
 import scalikejdbc.scalatest.AutoRollback
 
 @RunWith(classOf[JUnitRunner])
-class CheckSpec extends fixture.FunSpec with Matchers with DBSettings with AutoRollback {
+class CheckSpec extends fixture.FlatSpec with Matchers with DBSettings with AutoRollback {
 
   val name = "Scala"
 
@@ -41,32 +41,30 @@ class CheckSpec extends fixture.FunSpec with Matchers with DBSettings with AutoR
 
   }
 
-  describe("Bug") {
-    it("リズムを維持する") { implicit session =>
+  it should "リズムを維持する" in { implicit session =>
 
-      val a = Account.syntax("a")
-      val id = withSQL {
-        select(a.accountId).from(Account as a).where.eq(a.accountName, name)
-      }.map(_.long(1)).single().apply()
+    val a = Account.syntax("a")
+    val id = withSQL {
+      select(a.accountId).from(Account as a).where.eq(a.accountName, name)
+    }.map(_.long(1)).single().apply()
 
-      val status = "OPEN"
+    val status = "OPEN"
 
-      val (b, a1, a2, bs) = (Bug.syntax, Account.syntax("a1"), Account.syntax("a2"), BugStatus.syntax)
-      val bugs: List[Bug] = withSQL {
-        select
-          .from(Bug as b)
-          .join(Account as a1).on(b.reportedBy, a1.accountId)
-          .leftJoin(Account as a2).on(b.assignedTo, a2.accountId)
-          .join(BugStatus as bs).on(b.status, bs.status)
-          .where
-          .eq(b.assignedTo, id)
-          .and
-          .eq(b.status, status)
-      }.map(Bug(b, a1, a2, bs)).list().apply()
+    val (b, a1, a2, bs) = (Bug.syntax, Account.syntax("a1"), Account.syntax("a2"), BugStatus.syntax)
+    val bugs: List[Bug] = withSQL {
+      select
+        .from(Bug as b)
+        .join(Account as a1).on(b.reportedBy, a1.accountId)
+        .leftJoin(Account as a2).on(b.assignedTo, a2.accountId)
+        .join(BugStatus as bs).on(b.status, bs.status)
+        .where
+        .eq(b.assignedTo, id)
+        .and
+        .eq(b.status, status)
+    }.map(Bug(b, a1, a2, bs)).list().apply()
 
-      bugs should not(be(empty))
-      bugs.size should be(1)
-    }
+    bugs should not(be(empty))
+    bugs.size should be(1)
   }
 
 }
